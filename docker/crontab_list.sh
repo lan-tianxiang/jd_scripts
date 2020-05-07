af7056afb0b4ffa15621d3f29396d79dce7f9432
Lan <admin@highdimen.com>
2020-05-07 09:05:20 +0800

    .

---
+++ docker/crontab_list.sh
@@ -0,0 +1,22 @@
+
+0 13 * * * node /scripts/jd_carnivalcity_plus.js >> /scripts/logs/jd_carnivalcity_plus.js 2>&1
+
+0 14 * * * node /scripts/jd_entertainment.js >> /scripts/logs/jd_entertainment.js 2>&1
+
+0 15 * * * node /scripts/jd_inter_shopsign.js >> /scripts/logs/jd_inter_shopsign.js 2>&1
+
+0 16 * * * node /scripts/jd_mother_jump.js >> /scripts/logs/jd_mother_jump.js 2>&1
+
+0 17 * * * node /scripts/jd_paopao.js >> /scripts/logs/jd_paopao.js 2>&1
+
+0 18 * * * node /scripts/jd_shake.js >> /scripts/logs/jd_shake.js 2>&1
+
+0 19 * * * node /scripts/jd_shop_follow_sku.js >> /scripts/logs/jd_shop_follow_sku.js 2>&1
+
+0 20 * * * node /scripts/jd_shop_followsku.js >> /scripts/logs/jd_shop_followsku.js 2>&1
+
+0 21 * * * node /scripts/jd_shop_lottery.js >> /scripts/logs/jd_shop_lottery.js 2>&1
+
+0 22 * * * node /scripts/jd_xmf.js >> /scripts/logs/jd_xmf.js 2>&1
+
+0 23 * * * node /scripts/jx_cfdtx.js >> /scripts/logs/jx_cfdtx.js 2>&1
